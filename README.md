300ba8f3a4892a3dcc714bb3697efd3752b16356
masayuki.tanaka <masayuki.tanaka@diva.co.jp>
2021-01-14 15:44:46 +0900

    testing

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Shikibu
   
-日本語音声の書き起こしツールです。
+音声の書き起こしツールです。
+  
+Audio Transcriber Tool
+  
+## 環境設定
+
+[Azure Speech to Text](https://azure.microsoft.com/ja-jp/services/cognitive-services/speech-to-text/)のインスタンスを作成しkeyをAZURE_SPEECH_KEYとして環境変数に設定します
+  
+Prepare [Azure Speech to Text](https://azure.microsoft.com/ja-jp/services/cognitive-services/speech-to-text/) instance and set its key into environmental variable AZURE_SPEECH_KEY
+  
+## 前処理 pre-process
+  
+事前にwavファイルに変換してください
+  
+Change file format into .wav
+
+ex.
+  
+ffmpeg -i input.mp4 -ar 8000 output.wav
--- shikibu.py
+++ shikibu.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
-
 import speech_recognition as sr
-
-# obtain path to "english.wav" in the same folder as this script
-#from os import path
 import os
-AUDIO_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "english.wav")
+import sys
+
+#AUDIO_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "english.wav")
+#AUDIO_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test.mp4")
+AUDIO_FILE = sys.argv[1]
+LANGUAGE = sys.argv[2]
+print("AUDIOFILE = {}".format(AUDIO_FILE))
+print("LANGUAGE = {}".format(LANGUAGE))
+
 # use the audio file as the audio source
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
@@ -14,7 +17,7 @@ with sr.AudioFile(AUDIO_FILE) as source:
 # recognize speech using Microsoft Azure Speech
 AZURE_SPEECH_KEY = os.environ['AZURE_SPEECH_KEY']
 try:
-    print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY,location="japaneast"))
+    print("Microsoft Azure Speech thinks you said " + r.recognize_azure(audio, key=AZURE_SPEECH_KEY,location="japaneast",language=LANGUAGE))
 except sr.UnknownValueError:
     print("Microsoft Azure Speech could not understand audio")
 except sr.RequestError as e:
